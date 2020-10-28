49152808a549a510f60cf0a752d72e1b1ca0a9a6
Luka Kljajević <luka.kljajevic.dev@gmail.com>
2020-10-28 14:27:02 +0100

    started implementing english and russian languages

--- scripts/navbar.js
+++
@@ -1,8 +0,0 @@
-let url = window.location.pathname.split('/')[1];
-
-if (url === '')
-  url = 'index.html';
-
-$('#navbar').load('../partials/navbar.html', () => {
-  $(`.nav-link[href$='${url}']`).parent().addClass('active');
-});
--- scripts/partials.js
+++ scripts/partials.js
@@ -1,2 +1,33 @@
-$('#header').load('../partials/header.html');
-$('#footer').load('../partials/footer.html');
+let urlArray = window.location.pathname.split('/');
+let lang = 'rs';
+
+if (urlArray.length === 3) {
+  lang = urlArray[1]; 
+}
+
+// Header and Footer
+$('#header').load(`../partials/${lang}/header.html`);
+$('#footer').load(`../partials/${lang}/footer.html`);
+
+// Navbar
+let page = urlArray[urlArray.length - 1];
+if (page === '') page = 'index.html';
+
+$('#navbar').load(`../partials/${lang}/navbar.html`, () => {
+  $(`.nav-link[href$='${page}']`).parent().addClass('active');
+
+  $('.dropdown-menu a.dropdown-item').click(function() {
+    console.log(this.id);
+    console.log(window.location.pathname);
+    let targetLang = '';
+    
+    if (this.id !== 'rs') {
+      targetLang = `/${this.id}`;
+    }
+
+    window.location.pathname = `${targetLang}/${page}`;
+
+  });
+});
+
+
